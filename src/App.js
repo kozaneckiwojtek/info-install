b81383d2a9e6ed1f174c8cf0b6f4c6bc3f91f979
Wojtek <kozaneckiwojtek@gmail.com>
2023-07-15 22:21:39 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/App.css';
+import Baner from './baner';
+
 import Form from './form';
 function App() {
   return (
     <>
+    <Baner/>
     <Form/>
     </>
   );
---
+++ src/baner.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './css/style_baner.css'
+import baner from './img/Info-Install-logos_white.png'
+
+const Baner = () => {
+  return (
+    <>
+      <Container fluid className='baner'>
+        <Row>
+          <Col className='logo col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4'>
+            <a id="logotop" href="#top"><img src={baner} alt="logo" /></a>
+          </Col>
+          <Col className='navi col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8'>
+            <ul className="baner_list">
+              <li className="baner_elemen"><a href="#pointer-offer">Nasza Oferta</a></li>
+              <li className="baner_elemen"><a href="#pointer-portfolio">Nasze realizacje</a></li>
+              <li className="baner_elemen"><a href="#pointer-aboutUs">O nas</a></li>
+              <li className="baner_elemen"><a href="#contact">Kontakt</a></li>
+            </ul>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col className='hero col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12'>
+            <div className='image-container'>
+              <img src={baner} alt="Background" />
+              <div className="content">
+                <p>Rozwiązania elektryczne na miarę Twoich potrzeb</p>
+                <a href="#" id="open-button" className="cta-button">Skontaktuj się z nami</a>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+
+
+      
+    </>
+  );
+}
+
+export default Baner;
